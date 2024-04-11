9d668a25779fe6656832db20159b8c2b1607fa6c
junior <juniormathiias@gmail.com>
2024-04-11 20:21:35 -0300

    criei dois um component e pages tambem routes

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import * as S from './styles';
+
+function Header(){
+    return(
+        <div>
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+
+export default Header;
---
+++ src/routes/index.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// pages
+import Home from './../pages/Home';
+
+
+//components
+import Header from '../components/Header';
+
+
+
+function RoutesApp() {
+
+
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RoutesApp;
