66d28e3cc4801312a2de70a91c1b42a8951602dd
junior <juniormathiias@gmail.com>
2024-04-14 21:40:56 -0300

    added the icons and message

--- src/components/MessageList/index.jsx
+++ src/components/MessageList/index.jsx
@@ -1,25 +1,28 @@
-import { FaPercentage, FaTruck, FaCreditCard, FaShoppingCart } from 'react-icons/fa';
-
+import { FaCreditCard, FaShoppingCart } from 'react-icons/fa';
+import { MdOutlineDiscount } from "react-icons/md";
+import { LiaShippingFastSolid } from "react-icons/lia";
+import { TbStars } from "react-icons/tb";
 function MessageList() {
 
   return (
     <>
-      <section className="flex justify-around items-center py-8 bg-gray-100">
+      <section className="flex justify-center items-center py-2 mt-4">
         <div className="text-center">
-          <FaPercentage className="text-4xl text-blue-500 mb-2 mx-auto" />
-          <p className="text-lg">5% na primeira compra</p>
+          <MdOutlineDiscount className="text-4xl text-primary mb-2 mx-auto" />
+          <p className="text-lg text-secondary">5% na primeira compra</p>
         </div>
-        <div className="text-center">
-          <FaTruck className="text-4xl text-blue-500 mb-2 mx-auto" />
-          <p className="text-lg">Entrega: Correios</p>
+        <div className="text-center ml-4">
+          <LiaShippingFastSolid className="text-4xl text-primary mb-2 mx-auto" />
+          <p className="text-lg text-secondary">Entrega: Correios</p>
         </div>
-        <div className="text-center">
-          <FaCreditCard className="text-4xl text-blue-500 mb-2 mx-auto" />
-          <p className="text-lg">Parcelamos em até 5x</p>
+        <div className="text-center ml-4">
+          <FaCreditCard className="text-4xl text-primary mb-2 mx-auto" />
+          <p className="text-lg text-secondary">Parcelamos em até 5x</p>
         </div>
-        <div className="text-center">
-          <FaShoppingCart className="text-4xl text-blue-500 mb-2 mx-auto" />
-          <p className="text-lg">Atacado a partir de 15 peças</p>
+        <div className="text-center ml-4">
+          <FaShoppingCart className="text-4xl text-primary mb-2 mx-auto" />
+          {/* <TbStars className="text-4xl text-primary mb-2 mx-auto" /> */}
+          <p className="text-lg text-secondary">Atacado a partir de 15 peças</p>
         </div>
       </section>
     </>
